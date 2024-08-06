319c94ef47c4e1dcaabdb92603fc174a0d9b1ab9
mardon1dev <dusbekovmardonbek@gmail.com>
2024-08-06 13:34:10 +0500

    Change

--- js/script.js
+++ js/script.js
@@ -3,29 +3,59 @@ const resultEl = document.getElementById("result");
 const playerScoreEl = document.getElementById("user-score");
 const computerScoreEl = document.getElementById("computer-score");
 
+const recognition = new webkitSpeechRecognition();
+recognition.lang = "uz-UZ";
+recognition.start();
+recognition.onresult = function(e){
+  let tanlov = null;
+  let transcript = e.results[0][0].transcript;
+  tanlov = transcript;
+  buttons.forEach(button=>{
+    if (tanlov == transcript) {
+      if(button.id == transcript){
+        button.classList.add("opacity")
+      }
+      else{
+        button.classList.remove("opacity")
+      }
+    }
+  })
+  const result = playRound(transcript, computerPlay());
+  resultEl.textContent = result;
+}
+
+recognition.onend = function () {
+  recognition.start();
+};
+
+recognition.onerror = function (event) {
+  console.log('Speech recognition error: ', event.error);
+  resultEl.textContent = "Ovoz tanib olinmadi, qaytadan urinib ko'ring.";
+  recognition.start(); 
+};
+
 let playerScore = 0;
 let computerScore = 0;
 
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const result = playRound(button.id, computerPlay());
-    resultEl.textContent = result;
-  });
-});
-
 function computerPlay() {
-  const choices = ["tosh", "qog'oz", "qaychi"];
+  const choices = ["tosh", "qogʻoz", "qaychi"];
   const randomChoice = Math.floor(Math.random() * choices.length);
   return choices[randomChoice];
 }
 
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
+  const choices = ["tosh", "qogʻoz", "qaychi"];
+  if (!choices.includes(playerSelection)) {
+    return "Iltimos to'gri so'z ayting";
+  }
+
+  else if (playerSelection === computerSelection) {
     return "Durrang";
-  } else if (
+  } 
+  else if (
     (playerSelection === "tosh" && computerSelection === "qaychi") ||
-    (playerSelection === "qog'oz" && computerSelection === "tosh") ||
-    (playerSelection === "qaychi" && computerSelection === "qog'oz")
+    (playerSelection === "qogʻoz" && computerSelection === "tosh") ||
+    (playerSelection === "qaychi" && computerSelection === "qogʻoz")
   ) {
     playerScore++;
     playerScoreEl.textContent = playerScore;
